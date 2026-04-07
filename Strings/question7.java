120884422fd97956be783df5f398b3e43b97a522
yash-tech1801 <trivediyash026@gmail.com>
2026-04-07 22:23:00 +0530

    reverse word in a string

---
+++ Strings/question7.java
@@ -0,0 +1,21 @@
+package Strings;
+/*Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces. */
+class Solution {
+    public String reverseWords(String s) {
+        s = s.trim();
+        String[] str = s.split("\\s+");
+        int n = str.length - 1;
+        String[] ans = new String[str.length];
+        for (int i = 0; i < str.length; i++){
+            ans[i] = str[n - i];
+        }
+        String a = String.join(" ", ans);
+        return a;
+    }
+}
